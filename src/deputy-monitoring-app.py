3ec2e71bcb5d69aae8a95bbffac0347ee5ec7b3b
Renan Silva <renan.felipe39@gmail.com>
2021-06-08 00:41:40 -0300

    new info from deputy added

--- src/deputy-monitoring-app.py
+++ src/deputy-monitoring-app.py
@@ -6,7 +6,20 @@ import streamlit as st
 
 from support import Data
 
-deputy_list = list(Data.get_deputy_dict().keys())
+deputy_dict = Data.get_deputy_dict()
 
 st.header('Deputy monitoring')
-st.selectbox('Selecione o deputado:', deputy_list)
+deputy_id = st.selectbox('Selecione o deputado:', list(deputy_dict.keys()))
+
+col1, col2 = st.beta_columns(2)
+
+if deputy_id:
+    deputy_info = Data.get_deputy_info(deputy_dict[deputy_id])
+    col1.text('Name: {}'.format(deputy_info['ultimoStatus']['nome']))
+    col1.text('Party: {}'.format(deputy_info['ultimoStatus']['siglaPartido']))
+    col1.text('State: {}'.format(deputy_info['ultimoStatus']['siglaUf']))
+    col1.text('E-mail: {}'.format(deputy_info['ultimoStatus']['email']))
+    col1.text('Birth date: {}'.format(deputy_info['dataNascimento']))
+    col1.text('Education: {}'.format(deputy_info['escolaridade']))
+
+    col2.image(deputy_info['ultimoStatus']['urlFoto'], width=150)
