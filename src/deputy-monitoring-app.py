771727f710c5c234ef317b75c93d1b47e133843b
Renan Silva <renan.felipe39@gmail.com>
2021-06-18 00:28:14 -0300

    adding career information in a timeline format

--- src/deputy-monitoring-app.py
+++ src/deputy-monitoring-app.py
@@ -3,23 +3,37 @@
 #! Python3.8
 
 import streamlit as st
+from streamlit_timeline import timeline
 
 from support import Data
+
+# st.set_page_config(page_title="My App",layout='wide')
 
 deputy_dict = Data.get_deputy_dict()
 
 st.header('Deputy monitoring')
 deputy_id = st.selectbox('Selecione o deputado:', list(deputy_dict.keys()))
 
+st.markdown("""***""")
+
 col1, col2 = st.beta_columns(2)
 
 if deputy_id:
     deputy_info = Data.get_deputy_info(deputy_dict[deputy_id])
+    col1.header('Main information')
     col1.text('Name: {}'.format(deputy_info['ultimoStatus']['nome']))
     col1.text('Party: {}'.format(deputy_info['ultimoStatus']['siglaPartido']))
     col1.text('State: {}'.format(deputy_info['ultimoStatus']['siglaUf']))
     col1.text('E-mail: {}'.format(deputy_info['ultimoStatus']['email']))
-    col1.text('Birth date: {}'.format(deputy_info['dataNascimento']))
+    # col1.text('Birth date: {}'.format(deputy_info['dataNascimento']))
+    col1.text('Age: {}'.format(deputy_info['age']))
+    #col1.text('Today date: {}'.format(deputy_info['age']))
     col1.text('Education: {}'.format(deputy_info['escolaridade']))
 
-    col2.image(deputy_info['ultimoStatus']['urlFoto'], width=150)
+    col2.header('  ')
+    col2.image(deputy_info['ultimoStatus']['urlFoto'], width=180)
+
+    st.markdown("""***""")
+
+    deputy_timeline = Data.get_deputy_career(1)
+    timeline(deputy_timeline, height=400)
--- src/support.py
+++ src/support.py
@@ -3,6 +3,7 @@
 #! Python3.8
 
 import requests
+from datetime import date
 
 class Data:
 
@@ -29,9 +30,54 @@ class Data:
     def get_deputy_info(deputy_id):
         """This fuction return dictionary with main information from a specific deputy according to deputy_id parameter
         """
+        today = date.today()
 
         deputy_info_url_call = 'https://dadosabertos.camara.leg.br/api/v2/deputados/{}'.format(deputy_id)
         response = requests.get(deputy_info_url_call)
         data = response.json()
 
+        year, month, day = data['dados']['dataNascimento'].split('-')
+
+        birthday = date(int(year), int(month), int(day))
+
+        # data['dados']['age'] = round(int((today - birthday).days) / 365)
+        data['dados']['age'] = round(int((today - birthday).days) // 365.2425)
+
         return data['dados']
+
+    def get_deputy_career(deputy_id):
+        """This fuction return dictionary with main career information from a specific deputy according to deputy_id parameter
+        """
+        # deputy_info_url_call = 'https://dadosabertos.camara.leg.br/api/v2/deputados/{}/ocupacoes'.format(deputy_id)
+        # response = requests.get(deputy_info_url_call)
+        # data = response.json()
+
+        dd = {
+            "title": {
+                "media": {
+                "url": "",
+                "caption": "check",
+                "credit": ""
+                },
+                "text": {
+                "headline": "Career",
+                "text": "<p>Actually, jobs</p>"
+                }
+            },
+            "events": [
+                {
+                "media": {
+                    "url": "",
+                    "caption": "Evento test"
+                },
+                "start_date": {
+                    "year": "2010"
+                },
+                "text": {
+                    "headline": "Profissão test",
+                    "text": "<br/><p>Trampava bem.</p><br/>"
+                }
+                }            ]
+            }
+
+        return dd
