ccb6d189ef9567f3761cd4e94c6212d59b2a1039
rfelipesilva <42416868+rfelipesilva@users.noreply.github.com>
2021-08-05 22:12:38 -0300

    adding language option, PTBR & EN

--- src/deputy-monitoring-app.py
+++ src/deputy-monitoring-app.py
@@ -8,19 +8,26 @@ import streamlit as st
 import plotly.express as px
 
 from streamlit_timeline import timeline
+from support import Data, Language
 
-from support import Data
+st.set_page_config(page_title='Your Deputy', layout='wide', page_icon='🔍')
 
-st.set_page_config(page_title="My App",layout='wide')
-
+language = Language.get_lang_dict()
 deputy_dict = Data.get_deputy_dict()
+
+# #SETTING OWN THEME COLLOR
+# def local_css(file_name):
+#     with open(file_name) as f:
+#         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# # file_dir = '{}\\style.css'.format(os.path.abspath(os.getcwd()))
+# local_css('style.css')
 
 def get_bar_chart(dataframe):
     fig = px.line(dataframe, x='month', y='value', color='cost', width=1350)
     return fig
 
-def get_cost_charts(deputy_id):
-
+def get_cost_charts(deputy_id, language_dict):
     target_deputy_cost = Data.get_deputy_cost(deputy_id)
     full_deputies_cost = Data.get_full_cost()
 
@@ -35,49 +42,80 @@ def get_cost_charts(deputy_id):
 
     cost_data = pd.concat([target_deputy_cost, full_deputies_cost])
     cost_col1, cost_col2 = st.beta_columns(2)
-    cost_selected = cost_col1.selectbox('Select the cost type to analyze:',
+    cost_selected = cost_col1.selectbox('{}:'.format(language_dict['cost_section']['cost_type_filter']),
                                         list(target_deputy_cost.cost_type.unique()))
-    year_selected = cost_col2.selectbox('Select the year to analyze:', 
+
+    year_selected = cost_col2.selectbox('{}:'.format(language_dict['cost_section']['year_filter']), 
                                         list(target_deputy_cost.year.unique()))
+
     costs_df_filtered = cost_data.loc[(cost_data.cost_type == cost_selected) 
                                        & (cost_data.year == year_selected)]
-    st.write(costs_df_filtered)
+
     st.plotly_chart(get_bar_chart(costs_df_filtered))
 
-st.header('Deputy monitoring')
-deputy_id = st.selectbox('Selecione o deputado:', list(deputy_dict.keys()))
+# @st.cache
+def update_page(language_dict):
+    """Update app according with specific language
 
-st.markdown("""***""")
+    Args: 
+        language_dict (dictionary): Radio button option comingo from "selected_language" variable
 
-col1, col2 = st.beta_columns(2)
+    Returns: updated page according with user language selected
+    """
+    st.sidebar.header(language_dict['sidebar']['about']['header'])
+    st.sidebar.info(language_dict['sidebar']['about']['text'])
 
-if deputy_id:
-    deputy_info = Data.get_deputy_info(deputy_dict[deputy_id])
-    col1.header('Main information')
-    col1.text('Name: {}'.format(deputy_info['ultimoStatus']['nome']))
-    col1.text('Party: {}'.format(deputy_info['ultimoStatus']['siglaPartido']))
-    col1.text('State: {}'.format(deputy_info['ultimoStatus']['siglaUf']))
-    col1.text('E-mail: {}'.format(deputy_info['ultimoStatus']['email']))
-    # col1.text('Birth date: {}'.format(deputy_info['dataNascimento']))
-    col1.text('Age: {}'.format(deputy_info['age']))
-    #col1.text('Today date: {}'.format(deputy_info['age']))
-    col1.text('Education: {}'.format(deputy_info['escolaridade']))
-
-    col2.header('  ')
-    col2.image(deputy_info['ultimoStatus']['urlFoto'], width=180)
+    st.title('Your Deputy')
+    st.subheader(language_dict['first_section']['header'])
+    deputy_id = st.selectbox(language_dict['first_section']['desc'], list(deputy_dict.keys()))
 
     st.markdown("""***""")
-    st.header('Career information')
-    deputy_occupation = Data.get_deputy_occupation(deputy_dict[deputy_id])
-    st.text('Profession: {}'.format(deputy_occupation))
 
-    # st.write(Data.get_deputy_jobs(deputy_dict[deputy_id]))
+    col1, col2 = st.beta_columns(2)
 
-    deputy_timeline = Data.get_deputy_jobs(deputy_dict[deputy_id])
-    timeline(deputy_timeline, height=400)
+    if deputy_id:
+        deputy_info = Data.get_deputy_info(deputy_dict[deputy_id])
+        col1.header('{}'.format(language_dict['general_info_section']['header']))
+        col1.text('{}: {}'.format(language_dict['general_info_section']['name'],
+                                  deputy_info['ultimoStatus']['nome']))
 
-    st.markdown("""***""")
-    st.header('Cost analysis')
-    # deputy_costs = Data.get_deputy_costs(deputy_dict[deputy_id])
-    get_cost_charts(deputy_dict[deputy_id])
-    # st.write(deputy_costs)
+        col1.text('{}: {}'.format(language_dict['general_info_section']['party'],
+                                  deputy_info['ultimoStatus']['siglaPartido']))
+
+        col1.text('{}: {}'.format(language_dict['general_info_section']['state'],
+                                  deputy_info['ultimoStatus']['siglaUf']))
+
+        col1.text('E-mail: {}'.format(deputy_info['ultimoStatus']['email'])) #NO NEED TO USE DICT HERE
+
+        col1.text('{}: {}'.format(language_dict['general_info_section']['age'],
+                                   deputy_info['age']))
+
+        col1.text('{}: {}'.format(language_dict['general_info_section']['education'],
+                                         deputy_info['escolaridade']))
+
+        col2.header('  ')
+        col2.image(deputy_info['ultimoStatus']['urlFoto'], width=180)
+
+        st.markdown("""***""")
+        st.header('{}'.format(language_dict['career_section']['header']))
+        st.info('TO DO')
+        deputy_occupation = Data.get_deputy_occupation(deputy_dict[deputy_id])
+        st.text('{}: {}'.format(language_dict['career_section']['profession'],
+                                deputy_occupation))
+
+        deputy_timeline = Data.get_deputy_jobs(deputy_dict[deputy_id])
+        timeline(deputy_timeline, height=400)
+
+        st.markdown("""***""")
+        st.header('{}'.format(language_dict['cost_section']['header']))
+        st.info('TO DO MESSAGE')
+        st.text('{}'.format(language_dict['cost_section']['desc']))
+        get_cost_charts(deputy_dict[deputy_id], language_dict)
+
+st.sidebar.header('Language/Idioma')
+selected_language = st.sidebar.radio('', ['Portuguese/Português', 'English/Inglês'])
+
+if selected_language == 'Portuguese/Português':
+    update_page(language['pt'])
+else:
+    update_page(language['en'])
